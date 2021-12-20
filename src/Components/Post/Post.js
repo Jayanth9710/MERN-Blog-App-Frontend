a3f2d349472c1c360741889c496f7803a251ddc4
Jayanth9710 <kumarjayanth55@gmail.com>
2021-12-20 18:37:19 +0530

    image api

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -2,7 +2,7 @@ import './Post.css';
 import {Link} from 'react-router-dom'
 
 export default function Post({post}) {
-    const PF = "https://mern-blog-app-jay.herokuapp.com/";
+    const PF = "https://mern-blog-app-jay.herokuapp.com/images/";
     return (
         <div className="post">
             {post.photo && (
