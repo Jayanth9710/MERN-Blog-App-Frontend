940b9f82169e89f69d40ec83ac7e22565e5db362
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2023-03-26 00:13:55 +0530

    Updated API Endpoint

--- src/Settings.js
+++ src/Settings.js
@@ -1,7 +1,8 @@
 let getSettings = () => {
     return {
         // api:"http://localhost:7000"
-         api : "https://mern-blog-app-jay.herokuapp.com"
+//          api : "https://mern-blog-app-jay.herokuapp.com"
+        api:"https://mern-blog-backend-8xhy.onrender.com"
         
     }
 }
