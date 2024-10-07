9a9eaea426a2ad89828c57f5d3697528765822f4
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2024-10-07 22:08:45 +0530

    Update Settings.js

--- src/Settings.js
+++ src/Settings.js
@@ -2,7 +2,8 @@ let getSettings = () => {
     return {
         // api:"http://localhost:7000"
 //          api : "https://mern-blog-app-jay.herokuapp.com"
-        api:"https://mern-blog-backend-8xhy.onrender.com"
+        // api:"https://mern-blog-backend-8xhy.onrender.com"
+        api:"https://mern-blog-app-backend-alpha.vercel.app"
         
     }
 }
